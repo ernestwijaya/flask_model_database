7f02ada81163bf203254c954e4e7bf5a3d72137b
fellisaanastasya01 <ernestwijaya_2428250010@mhs.mdp.ac.id>
2025-11-27 18:24:54 +0700

    flask_model_database

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+loaded_model = joblib.load("models/model_diabetes.pkl")
+loaded_scaler = joblib.load("models/standar_scaler.pkl")
+columns = [
+    'Pregnancies',
+    'Glucose',
+    'BloodPressure',
+    'SkinThickness',
+    'Insulin',
+    'BMI',
+    'DiabetesPedigreeFunction',
+    'Age'
+]
+
+@app.route('/')
+def index():
+    return jsonify({
+        "meta" : {
+            "status" : "Success",
+            "message" : "Welcome to Diabetes API"
+        },
+        "data" : None
+    })
+
+@app.route('/api/predict', methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    X_input = pd.DataFrame([data], columns=columns)
+
+    X_input_scaled =  loaded_scaler.transform(X_input)
+
+    prediction = loaded_model.predict(X_input_scaled)
+
+    return jsonify({
+        "meta": {
+            "status": "Success",
+            "message": "Prediction"
+        },
+        "data": "Positive" if prediction.tolist()[0] == 1 else "Negative"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
